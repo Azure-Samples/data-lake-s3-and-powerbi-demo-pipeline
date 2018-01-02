df97fe9fad319971372e8b706a3f9c8fca16771f
mabasile-MSFT <34490972+mabasile-MSFT@users.noreply.github.com>
2018-01-02 11:56:11 -0800

    Changing text and reorganizing to improve clarity

--- AllADFPowershellScripts.ps1
+++ AllADFPowershellScripts.ps1
@@ -1,10 +1,18 @@
-#This file contains all of the Powershell scripts necessary to create and run the Azure Data Factory pipeline to copy data from an AWS S3 account to an ADLS account.
-#Before running these scripts, ensure you are logged into your Azure account and subscription by running Login-AzureRmAccount and Select-AzureRmSubscription
-#Also check that you have edited the corresponding JSON files (located in the GitHub under ADF_Json_files) with the necessary credentials and parameters to match your ADF account and resource group
+#Below are all of the Powershell scripts necessary to create and run an Azure Data Factory pipeline to copy data from an AWS S3 account to an ADLS account, as explained in the referenced blog post. Before running these scripts complete the following:
+### 1. Fill in all undefinied variables within the <>'s
+### 2. Open a Powershell window and run Login-AzureRmAccount, viewing your subscriptions with Get-AzureRmSubscription, and choosing one with Select-AzureRmSubscription -SubscriptionId <your subscription id> 
+### 3. Download the ADF defintion files under /ADF_Json_files/ in this Github
+### 4. Generate the access keys for your AWS and ADLS account and add them into /ADF_Json_files/AmazonS3LinkedService.json and /ADF_Json_files/ADLSLinkedService.json - see blog for more detail
 
-#Define all object names here. Included are some default names to match the JSON files, edit them as necessary to match your own files.  
+#Run each group of scripts individually to ensure child objects are created after their parent object. Copy and paste each group into your powershell window.
+
+#Group 0: Define all parameters.
+##Define personal variables for your Azure account
+$ADFpath = "<path to location of the ADF JSON files>"
 $resourceGroupName = "<your resource group>"
 $dataFactoryName = "<your data factory>"
+
+#Define all ADF object names
 $integrationRuntimeName = "AWSToADLSIntegrationRuntime"
 $ADLSLinkedServiceName = "ADLSLinkedService"
 $AmazonS3LinkedServiceName = "AmazonS3LinkedService"
@@ -12,32 +20,36 @@ $ADLSDatasetName = "ADLSCopyDataset"
 $AmazonS3DatasetName = "AmazonS3CopyDataset"
 $AWSToADLSPipelineName = "AWSToADLSCopyPipeline"
 
-#Define the relevant JSON definition file names for each object
+##Define the relevant JSON definition file names for each object
 $ADLSLinkedServiceFile = $ADLSLinkedServiceName + ".json"
 $AmazonS3LinkedServiceFile = $AmazonS3LinkedServiceName + ".json"
 $ADLSDatasetFile = $ADLSDatasetName + ".json"
 $AmazonS3DatasetFile = $AmazonS3DatasetName + ".json"
 $AWSToADLSPipelineFile = $AWSToADLSPipelineName + ".json"
 
-#Run each group of these scripts individually by copy+pasting into PowerShell window to ensure that each object is created before its dependents.
-
 #Group 1: Create the integration runtime and linked services
+cd $ADFpath
+#Integration runtime
 Set-AzureRmDataFactoryV2IntegrationRuntime -ResourceGroupName $resourceGroupName -DataFactoryName $dataFactoryName -Name $integrationRuntimeName -Location "East US 2"
+#S3 Linked Service
 Set-AzureRmDataFactoryV2LinkedService -ResourceGroupName $resourceGroupName -DataFactoryName $dataFactoryName -Name $AmazonS3LinkedServiceName -DefinitionFile $AmazonS3LinkedServiceFile
+#ADLS Linked Service
 Set-AzureRmDataFactoryV2LinkedService -ResourceGroupName $resourceGroupName -DataFactoryName $dataFactoryName -Name $ADLSLinkedServiceName -DefinitionFile $ADLSLinkedServiceFile
 
 #Group 2: Create the datasets
+##S3 Dataset
 Set-AzureRmDataFactoryV2Dataset -ResourceGroupName $resourceGroupName -DataFactoryName $dataFactoryName -Name $AmazonS3DatasetName -DefinitionFile $AmazonS3DatasetFile
+##ADLS Dataset
 Set-AzureRmDataFactoryV2Dataset -ResourceGroupName $resourceGroupName -DataFactoryName $dataFactoryName -Name $ADLSDatasetName -DefinitionFile $ADLSDatasetFile
 
 #Group 3: Create the pipeline
 Set-AzureRmDataFactoryV2Pipeline -ResourceGroupName $resourceGroupName -DataFactoryName $dataFactoryName -Name $AWSToADLSPipelineName -DefinitionFile $AWSToADLSPipelineFile
 
-#Group 4: Run and monitor the pipeline
-#Run the pipeline
+#Group 4: Run the pipeline
+##Run the pipeline
 $runId = Invoke-AzureRmDataFactoryV2Pipeline -ResourceGroupName $resourceGroupName -DataFactoryName $dataFactoryName -PipelineName $AWSToADLSPipelineName
 
-#Monitor the pipeline until it finishes copying data
+#Group 5: Monitor the pipeline until it returns a successful complete message
 while ($True) {
     $Run = Get-AzureRmDataFactoryV2PipelineRun -ResourceGroupName $resourceGroupName -DataFactoryName $dataFactoryName -PipelineRunId $runId
 
